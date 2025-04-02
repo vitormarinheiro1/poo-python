6bc2133fd0c5bc51848446a41922df81a9e3711d
Vitor Marinheiro Rodrigues <x396751@prodam.pref>
2025-04-02 14:52:18 -0300

    refactor setup

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+from contas import ContaCorrente, ContaPoupanca
 
-
-class Pessoa:
-    ano = 2025
-
-    def __init__(self,nome,idade):
-        self.nome = nome
-        self.idade = idade
-
-    @classmethod
-    def metodo_de_classe(cls):
-        print('hey')
-
-# p1 = Pessoa('Joao', 53)
-Pessoa.metodo_de_classe()
+if __name__ == '__main__':
+    cp1 = ContaPoupanca(111, 222)
+    cp1.sacar(1)
+    cp1.depositar(1)
+    cp1.sacar(1)
+    print('##')
+    cc1 = ContaCorrente(111, 222)
+    cc1.sacar(1)
+    cc1.depositar(1)
+    cc1.sacar(1)
